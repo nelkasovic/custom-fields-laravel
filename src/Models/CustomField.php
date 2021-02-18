976b06106efc6c986b8e384167d10e04321cb676
Elkasovic Nermin <nermin.elkasovic.ext@adcubum.com>
2021-02-18 06:56:59 +0100

    Update on last commit

--- src/Models/CustomField.php
+++ src/Models/CustomField.php
@@ -76,6 +76,9 @@ class CustomField extends Model
     {
         parent::boot();
         self::creating(function ($field) {
+            if ($field && $field->model) {
+                $lastFieldOnCurrentModel = $field->model->customFields()->orderBy('order', 'desc')->first();
+            }
             $lastFieldOnCurrentModel = $field->model->customFields()->orderBy('order', 'desc')->first();
             $field->order = ($lastFieldOnCurrentModel ? $lastFieldOnCurrentModel->order : 0) + 1;
         });
